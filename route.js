763678b5d2b7510f7e8056835f2444b60e3b6255
sixolisiwe <sxlswmcb@gmail.com>
2019-11-20 09:36:47 +0200

    route.js

--- route.js
+++ route.js
@@ -1,8 +1,9 @@
 module.exports = function names(toBeBooked) {
 
     async function enteredNumbers(req, res) {
+        let name = req.body.textBox
 
-        let waiters = await toBeBooked.usersBooked();
+        let waiters = await toBeBooked.usersBooked(name);
         res.render('index', {
             waiter_App: waiters
      
